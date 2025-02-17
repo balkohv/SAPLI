cec413204b40de8c2c8e71239d62404a3dd3e3b6
balkohv <97893750+balkohv@users.noreply.github.com>
2025-02-17 19:11:44 +0100

    Update connexion.php

--- sapli-api/sapli-auth/connexion.php
+++ sapli-api/sapli-auth/connexion.php
@@ -1,6 +1,6 @@
 <?php
-    $username ="root";
-    $pwd = "Micheldu15*"; 
+    $username ="";
+    $pwd = ""; 
     try {
         $db = new PDO('mysql:host=192.168.1.59;port=3306;dbname=api_user',$username,$pwd);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
